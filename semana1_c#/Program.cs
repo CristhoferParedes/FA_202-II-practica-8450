742ba7d3b90a18956bae4b0143c4d3f80d6d796f
cristhofer <cristhofer@gmail.com>
2025-09-02 15:30:54 -0500

    subiendo ejercicio1-sem1

---
+++ semana1_c#/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace semana1_c_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ejer1();
+        }
+
+        static void ejer1()
+        {
+            string nombre, carrera;
+
+            Console.Write("ingrese su nombre: ");
+            nombre = Console.ReadLine();
+
+            Console.Write("ingrese su carrera: ");
+            carrera = Console.ReadLine();
+
+            Console.WriteLine($"{nombre}, Bienvenido a FA de {carrera}");
+        }
+        static void ejer2()
+        {
+
+        }
+        static void ejer3()
+        {
+
+        }
+        static void ejer4()
+        {
+
+        }
+        static void ejer5()
+        {
+
+        }
+        static void ejer6()
+        {
+
+        }
+    }
+}
---
+++ semana1_python/semana1_python.py
@@ -0,0 +1,7 @@
+def ejer1():
+    nombre = input("ingrese su name: ")
+    carrera = input("ingrese su caerra: ")
+
+    print(f"{nombre}, bienbenido a FA de {carrera}")
+
+ejer1()
